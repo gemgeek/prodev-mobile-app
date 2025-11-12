a421d6d140dec96390ccb234c5046c7337372499
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-11-12 11:07:56 +0000

    Task 3: Implement NativeWind styling for Profile page

---
+++ prodev-mobile-app-0x06/app/join.tsx
@@ -0,0 +1,75 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_join"; 
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import { HEROLOGOGREEN, GOOGLELOGO, FACEBOOKLOGO } from "@/constants";
+
+export default function JoinScreen() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.iconsection}>
+          <Link href="/" asChild>
+            <Ionicons name="arrow-back" size={25} />
+          </Link>
+          <Image source={HEROLOGOGREEN} />
+        </View>
+
+        <View style={styles.titleTextGroup}>
+          <Text style={styles.titleText}>Create your</Text>
+          <Text style={styles.titleText}>Account</Text>
+          <Text style={styles.subText}>
+            Create an account to start your journey.
+          </Text>
+        </View>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.formLabel}>Full Name</Text>
+            <TextInput style={styles.formControl} />
+          </View>
+          <View>
+            <Text style={styles.formLabel}>Email</Text>
+            <TextInput keyboardType="email-address" style={styles.formControl} />
+          </View>
+          <View>
+            <Text style={styles.formLabel}>Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput style={styles.passwordControl} secureTextEntry={true} />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.buttonText}>Join Now</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        <View style={styles.secondaryButtonGroup}>
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={GOOGLELOGO} />
+            <Text style={styles.secondaryButtonText}>Sign up with Google</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={FACEBOOKLOGO} />
+            <Text style={styles.secondaryButtonText}>Sign up with Facebook</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.signupgroup}>
+          <Text style={styles.signupTitleText}>Already have an account?</Text>
+          <Link href="/signin" asChild>
+            <Text style={styles.signupSubTitleText}>Sign in</Text>
+          </Link>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
