b023473b663ed0630d484998a919f5a5a2b5488f
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-11-12 10:16:50 +0000

    Fix: Correct PropertyListing file path and verify layouts

--- prodev-mobile-app-0x05/app/_layout.tsx
+++ prodev-mobile-app-0x05/app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import { View } from 'react-native'
 
 export default function RootLayout() {
   return (
@@ -9,7 +8,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
-      <Stack.Screen name="(home)" />
+      <Stack.Screen name="(home)" /> 
     </Stack>
   );
 }
